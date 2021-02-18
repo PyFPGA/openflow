e5984c713aa0e3f3808feb678d94ab8c28242af1
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2021-02-18 11:30:26 -0300

    openflow: add get_part function
    And first prototypes for the class constructor and the methods 'synthesis',
    'implementation' and 'bitstream'.

--- openflow/openflow.py
+++ openflow/openflow.py
@@ -24,6 +24,88 @@ A Python Package which solves HDL-to-bitstream based on FOSS.
 class Openflow:
     """Class which solves HDL-to-bitstream based on FOSS."""
 
-    def __init__(self):
+    def __init__(self, name='openflow', part='hx8k-ct256'):
         """Class constructor."""
-        print('Hello from Openflow')
+        self.name = name
+        self.part = get_part(part)
+
+    # pylint: disable=too-many-arguments
+    def synthesis(
+            self, top,
+            vhdl=None, verilog=None, system_verilog=None,
+            arch=None, defines=None, params=None, paths=None):
+        """Performs synthesis."""
+        print(self.name)
+        print(self.part)
+        print(top)
+        print(vhdl)
+        print(verilog)
+        print(system_verilog)
+        print(arch)
+        print(defines)
+        print(params)
+        print(paths)
+
+    def implementation(self, constraint=None):
+        """Performs implementation."""
+        print(self.name)
+        print(self.part)
+        print(constraint)
+
+    def bitstream(self):
+        """Performs bitstream generation."""
+        print(self.name)
+        print(self.part)
+
+
+def get_part(part):
+    """Get info about the FPGA part.
+
+    :param part: the FPGA part as specified by the tool
+    :returns: a dictionary with the keys name, family, device and package
+    """
+    name = part.lower()
+    # Looking for the family
+    family = None
+    families = [
+        # From <YOSYS>/techlibs/xilinx/synth_xilinx.cc
+        'xcup', 'xcu', 'xc7', 'xc6s', 'xc6v', 'xc5v', 'xc4v', 'xc3sda',
+        'xc3sa', 'xc3se', 'xc3s', 'xc2vp', 'xc2v', 'xcve', 'xcv'
+    ]
+    for item in families:
+        if name.startswith(item):
+            family = item
+    families = [
+        # From <nextpnr>/ice40/main.cc
+        'lp384', 'lp1k', 'lp4k', 'lp8k', 'hx1k', 'hx4k', 'hx8k',
+        'up3k', 'up5k', 'u1k', 'u2k', 'u4k'
+    ]
+    if name.startswith(tuple(families)):
+        family = 'ice40'
+    families = [
+        # From <nextpnr>/ecp5/main.cc
+        '12k', '25k', '45k', '85k', 'um-25k', 'um-45k', 'um-85k',
+        'um5g-25k', 'um5g-45k', 'um5g-85k'
+    ]
+    if name.startswith(tuple(families)):
+        family = 'ecp5'
+    # Looking for the device and package
+    device = None
+    package = None
+    aux = name.split('-')
+    if len(aux) == 2:
+        device = aux[0]
+        package = aux[1]
+    elif len(aux) == 3:
+        device = '{}-{}'.format(aux[0], aux[1])
+        package = aux[2]
+    else:
+        raise ValueError('Part must be DEVICE-PACKAGE')
+    if family == 'ice40' and device.endswith('4k'):
+        # See http://www.clifford.at/icestorm/
+        device = device.replace('4', '8')
+        package += ":4k"
+    # Finish
+    return {
+        'name': name, 'family': family, 'device': device, 'package': package
+    }
